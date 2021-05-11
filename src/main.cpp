5f72d1a8c4c699a0d7177d5ee81a0cc7f0a088e6
sla-ppy <jerikolo1@gmail.com>
2021-05-11 20:39:02 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ncurses.h>
+
+WINDOW *create_newwin(int height, int width, int starty, int startx);
+void destroy_win(WINDOW *local_win);
+
+int main() {
+
+    initscr();
+    printw("Hello World !!!");
+    refresh();
+    getch();
+    // restore terminal settings before exit
+    endwin();
+
+    return 0;
+}
