c84724ae331b0591b93a6f90b30396ad8699a82e
sla-ppy <jerikolo1@gmail.com>
2021-05-14 01:51:24 +0200

    inputManager(), resizeTest(), testExample()

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,63 @@
 #include <ncurses.h>
+#include <string.h>
 
-int main() {
+// inserting example from the manual
+void testExample() {
+    char msg[] = "Just a string";        /* message to be appeared on the screen */
+    int row, col;                /* to store the number of rows and *
+					 * the number of columns of the screen */
+    initscr();                /* start the curses mode */
+    getmaxyx(stdscr, row, col);        /* get the number of rows and columns */
+    mvprintw(row / 2, (col - strlen(msg)) / 2, "%s", msg);
+    /* print the message at the center of the screen */
+    mvprintw(row - 2, 0, "This screen has %d rows and %d columns\n", row, col);
+    printw("Try resizing your window(if possible) and then run this program again");
+    refresh();
+    getch();
+    endwin();
+}
 
-    // CURSES BASE SETTINGS:
-    initscr(); // starts curses mode
-    keypad(stdscr, TRUE); // enables reading of F1, F2, arrow keys, etc
-    raw(); // ctrl characters are directly passed to the program without generating a signal
-    noecho();
+// awful code
+// basically tried to use std::vector to always be able to change the message and constantly resize the window at the same time, didn't work
+void resizeTest() {
+    char msg[] = "Hi, I am the center of the terminal."; // msg to appear on screen
+    int row, col;
 
-    printw("Press F2 to print Hello!\n" );
-    printw("Press the arrow keys to simulate movement.\n");
-    printw("Press q to quit the program.\n");
+    getmaxyx(stdscr, col, row); // get number of rows and cols
 
-    bool terminalIsRunning{true};
+    if (row > 20 && col > 10) {
+        mvprintw(row / 2, (col - strlen(msg)) / 2, "%s", msg);
+        mvprintw(row - 2, 0, "This screen has %d rows and %d columns\n", row, col);
+        printw("Try resizing your window(if possible) and then run this program again");
+        refresh();
+    }
+    if (row > 40 && col > 20) {
+        mvprintw(row / 2, (col - strlen(msg)) / 2, "%s", msg);
+        mvprintw(row - 2, 0, "This screen has %d rows and %d columns\n", row, col);
+        printw("Try resizing your window(if possible) and then run this program again");
+        refresh();
+    }
+    if (row > 60 && col > 30) {
+        mvprintw(row / 2, (col - strlen(msg)) / 2, "%s", msg);
+        mvprintw(row - 2, 0, "This screen has %d rows and %d columns\n", row, col);
+        printw("Try resizing your window(if possible) and then run this program again");
+        refresh();
+    }
+    if (row > 80 && col > 40) {
+        mvprintw(row / 2, (col - strlen(msg)) / 2, "%s", msg);
+        mvprintw(row - 2, 0, "This screen has %d rows and %d columns\n", row, col);
+        printw("Try resizing your window(if possible) and then run this program again");
+        refresh();
+    }
+}
 
-    while (terminalIsRunning)
-    {
-        refresh(); // updates stdscr so we can see the new outputs
+bool inputManager() {
+    int keyInput;
 
-        int keyInput;
+    bool inputBeingManaged{true};
+
+    while (inputBeingManaged) {
+
         keyInput = getch();
 
         // how to use F1-12 keys
@@ -30,10 +69,11 @@ int main() {
         if (keyInput == 'q') {
             printw("Quitting now...\n");
             getch();
-            terminalIsRunning = false;
+            inputBeingManaged = false;
         }
 
         // movement prototype
+        // FIXME: interestingly when you scroll up and down, it also works as KEY_UP and KEY_DOWN, why?
         if (keyInput == KEY_LEFT) {
             printw("We moved left.\n");
         }
@@ -46,6 +86,37 @@ int main() {
         if (keyInput == KEY_DOWN) {
             printw("We moved down.\n");
         }
+
+        // test example
+        if (keyInput == 't') {
+            testExample();
+        }
+    }
+    return true;
+}
+
+int main() {
+
+    // CURSES BASE SETTINGS:
+    initscr(); // starts curses mode
+    keypad(stdscr, TRUE); // enables reading of F1, F2, arrow keys, etc
+    raw(); // ctrl characters are directly passed to the program without generating a signal
+    noecho();
+
+    printw("<F2> - print Hello!\n");
+    printw("<Arrow keys> - simulate movement\n");
+    printw("<q> - Quit\n");
+    printw("<t> - Test example\n");
+
+    bool terminalIsRunning{true};
+
+    while (terminalIsRunning) {
+        refresh(); // updates stdscr so we can see the new outputs
+
+        resizeTest();
+        inputManager();
+
+        terminalIsRunning = false;
     }
 
     endwin(); // ends curses mode
